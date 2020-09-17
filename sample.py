5896b64978e07c7aec699a6e8f25f87ce9e06ffd
Ranganayakulu <rangusivaranganayakulu@gmail.com>
2020-09-17 12:25:38 +0530

    new files added

---
+++ sample.py
@@ -0,0 +1,14 @@
+def even(n):
+    if n%2==0:
+        return True
+    else:
+        return False
+
+def add(a,b):
+    s = a+b
+    return s
+def sub(a,b):
+    d = a-b
+    return d
+def mul(s,h):
+    return s*h
